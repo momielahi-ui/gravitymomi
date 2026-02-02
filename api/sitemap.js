e398b1d1e13f4ecccd66bce7ed50b67790baacda
Momi Elahi <momielahi@gmail.com>
2026-02-03 04:41:35 +0500

    fix: remove XSLT stylesheet to serve pure XML to bots

--- api/sitemap.js
+++ api/sitemap.js
@@ -69,7 +69,6 @@ export default function handler(req, res) {
 
     // 4. Generate XML
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${allUrls.map(page => `  <url>
     <loc>${page.loc}</loc>
